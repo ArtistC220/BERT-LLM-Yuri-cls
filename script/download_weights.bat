ddc445638b05f98a84a7617a911e825d92d604a9
ArtistC220 <1971639508@qq.com>
2025-10-18 17:06:41 +0800

    Remove unused files

--- script/download_weights.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-echo === Downloading pre-trained weights ===
-curl -L -o models\BERT-Yuri-CLS.zip ^
-  https://github.com/ArtistC220/BERT-LLM-Yuri-cls/releases/download/v1.0.0/BERT-Yuri-CLS.zip
-powershell -command "Expand-Archive -Force models\BERT-Yuri-CLS.zip models\"
-del models\BERT-Yuri-CLS.zip
-echo Done!

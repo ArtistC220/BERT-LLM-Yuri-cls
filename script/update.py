9313a39f19321d8a45704f42d24052bc9cb80072
ArtistC220 <1971639508@qq.com>
2026-02-13 18:38:01 +0800

    在更新时将不会删除测试文件夹txt_test中的测试文本

--- script/update.py
+++ script/update.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     target_folders = [path_1, path_2]
 
     keep = ["ph.md","stopwords.txt", "verbword.txt", "baihe_aid_title_cover_local.csv", "filename_realvolume.csv" , "u-requirements.txt"]
-    keep_subfolders = ["history_rank_volume", "history_rank_book","result01","result02","txt_cleaned","txt_val_cleaned1","txt_train_cleaned1","normalizerCSV"]  
+    keep_subfolders = ["txt_test","history_rank_volume", "history_rank_book","result01","result02","txt_cleaned","txt_val_cleaned1","txt_train_cleaned1","normalizerCSV"]  
 
     for folder in target_folders:
         clean_folder_recursive(folder, keep_files=keep, keep_dirs=keep_subfolders)
